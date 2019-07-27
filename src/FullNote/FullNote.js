6d84767613fc49e6f065702053c6bd963f8e33e0
Keeley <keeleyduffey@gmail.com>
2019-07-27 14:31:51 -0600

    Removes old + unused error boundary

--- src/FullNote/FullNote.js
+++ src/FullNote/FullNote.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Note from '../Note/Note';
 import NoteDetail from '../NoteDetail/NoteDetail';
 import NotefulContext from '../NotefulContext';
-import MainSectionError from '../ValidationError/ValidationError';
 import {getNote} from '../helper-functions';
 
 class FullNote extends Component {
@@ -14,12 +13,10 @@ class FullNote extends Component {
     const note = getNote(notes, noteId);
 
     return (
-      <MainSectionError>
         <div>
           <Note {...note} />
           <NoteDetail {...note} />
         </div>
-      </MainSectionError>
     );
   }
 }
